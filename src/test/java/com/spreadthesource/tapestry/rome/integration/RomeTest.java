2b54daf202c5b1806982904051a34aafa327583f
Robin Komiwes <odisseus@gmail.com>
2010-04-09 13:01:04 +0200

    fixed surefire plugin configuration. Added tests

--- src/test/java/com/spreadthesource/tapestry/rome/integration/RomeTest.java
+++ src/test/java/com/spreadthesource/tapestry/rome/integration/RomeTest.java
@@ -7,10 +7,18 @@ import org.testng.annotations.Test;
 public class RomeTest extends SeleniumTestCase
 {   
     @Test
-    public void lala()
+    public void atom()
     {
-        open("index");
+        open("/index:atom");
         
-       // System.out.println(getText("//html"));
+        assertTrue(getBodyText().contains("my Atom feed"));
+    }
+    
+    @Test
+    public void rss()
+    {
+        open("/index:rss");
+        assertTrue(getBodyText().contains("my RSS feed"));
+
     }
 }
